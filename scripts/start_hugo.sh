3611ae807881f6731dd0ee5f53c6e8ff48abd106
Alberto Lagos <alberto.lagos@gmail.com>
2026-05-27 17:00:53 -0400

    Update code.

--- scripts/start_hugo.sh
+++ scripts/start_hugo.sh
@@ -1,5 +1,6 @@
-startx /usr/bin/dosbox -- \
--c " mount c /opt/hugo-phone/games" \
--c " imgmount d /opt/hugo-phone/games/hugo/hugo4.iso -t iso" \
--c " d:" \
--c " INSTALL.EXE"
+#!/bin/bash
+exec /usr/bin/dosbox \
+-c "mount c /opt/hugo-phone/games" \
+-c "imgmount d /opt/hugo-phone/games/hugo/hugo4.iso -t iso" \
+-c "d:" \
+-c "INSTALL.EXE"
